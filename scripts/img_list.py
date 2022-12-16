c61317ee3e18ba5f51d8694bbab78a43698dbab9
Pranit Zope <pranit.t.zope@gmail.com>
2022-12-16 23:46:34 +0530

    added aruco detection and misc models

--- scripts/img_list.py
+++ scripts/img_list.py
@@ -6,12 +6,24 @@ import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
- 
-
+import cv2.aruco as aruco
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 def img_callback(data):
     br=CvBridge()
     cur_frame=br.imgmsg_to_cv2(data)
-    cv2.circle(cur_frame,(150,150),5, (0, 0, 255), 4)
+    gray = cv2.cvtColor(cur_frame, cv2.COLOR_BGR2GRAY)
+    corners, ids, _ = aruco.detectMarkers(gray, aruco_dict)
+    if corners:
+        # Draw a bounding box around each ArUco marker
+        aruco.drawDetectedMarkers(cur_frame, corners)
+
+        # Print the number of ArUco markers detected
+        rospy.loginfo("Number of ArUco markers detected: "+  str(len(corners)))
+
+        # Print the IDs and locations of the ArUco markers
+        for i in range(len(corners)):
+            print("ID: ", ids[i], " Location: ", corners[i])
+    
     cv2.imshow('feed',cur_frame)
     cv2.waitKey(1)
 
---
+++ scripts/simple_aruco_detect.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+import cv2.aruco as aruco
+
+# Create a dictionary of ArUco markers
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+
+# Create a video capture object
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Capture a frame from the video
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect ArUco markers in the frame
+    corners, ids, _ = aruco.detectMarkers(gray, aruco_dict)
+
+    # If at least one ArUco marker was detected, draw a bounding box around it
+    if corners:
+        # Draw a bounding box around each ArUco marker
+        aruco.drawDetectedMarkers(frame, corners)
+
+        # Print the number of ArUco markers detected
+        print("Number of ArUco markers detected: ", len(corners))
+
+        # Print the IDs and locations of the ArUco markers
+        for i in range(len(corners)):
+            print("ID: ", ids[i], " Location: ", corners[i])
+
+    # Display the resulting frame
+    cv2.imshow('Frame', frame)
+
+    # Press 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Clean up
+cap.release()
+cv2.destroyAllWindows()
